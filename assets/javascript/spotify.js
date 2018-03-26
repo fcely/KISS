579bf08936d3399bbe0e79beba56aa45642e7835
Fernando Cely <fercely1@gmail.com>
2018-03-26 14:58:09 -0700

    alert

--- assets/javascript/spotify.js
+++ assets/javascript/spotify.js
@@ -1,5 +1,7 @@
 var string=location.href
 
+alert(string)
+
 if (string=== "https://fcely.github.io/KISS/spotify.html"){
 
 document.location.href="https://accounts.spotify.com/authorize/?client_id=4bfc50b0cfd8462c80ed1240b4bdd6a7&response_type=token&redirect_uri=https://fcely.github.io/KISS/spotify.html&scope=user-read-private%20user-read-email&state=34fFs29kd09"
